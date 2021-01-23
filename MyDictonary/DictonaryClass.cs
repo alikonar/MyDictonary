adaf3ba1fcbd7e4c84ee06566724cb32bf15a63a
Ali Konar <ali.knr@hotmail.com>
2021-01-23 16:38:50 +0300

    Add project files.

---
+++ MyDictonary/DictonaryClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictonary
+{
+    class DictonaryClass<TVal, TKey>
+    {
+        TVal[] vals;
+        TKey[] keys;
+
+        public DictonaryClass()
+        {
+            vals = new TVal[0];
+            keys = new TKey[0];
+        }
+
+        public void Add(TVal val, TKey key)
+        {
+            TVal[] tempVals = vals;
+            TKey[] tempKeys = keys;
+
+
+            vals = new TVal[vals.Length + 1];
+            keys = new TKey[keys.Length + 1];
+
+            for (int i = 0; i < tempVals.Length; i++)
+            {
+                vals[i] = tempVals[i];
+                keys[i] = tempKeys[i];
+                
+
+            }
+            vals[vals.Length - 1] = val;
+            keys[keys.Length - 1] = key;
+
+       
+
+        }
+        
+        public int Count
+        {
+            get { return vals.Length; }
+        }
+
+    }
+       
+       
+
+
+
+    
+}
---
+++ MyDictonary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DictonaryClass<string, int> dictonaryClass = new DictonaryClass<string, int>();
+
+            dictonaryClass.Add("Mantı", 200);
+            dictonaryClass.Add("Tavuk", 300);
+            dictonaryClass.Add("Sucuk", 400);
+            dictonaryClass.Add("Ekmem", 500);
+
+
+            Console.WriteLine(dictonaryClass.Count);
+
+        }
+    }
+}
